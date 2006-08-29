e183b1f957efee1ae7d51b9a0b1f67aa0efa5683
Peter Walther <pewa-github@rosasurfer.com>
2006-08-29 13:55:43 +0000

    *** empty log message ***

--- bin/cvs-update.sh
+++ bin/cvs-update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-PROJECT_ROOT=/var/www
-PROJECT_NAME=php_lib
+PROJECTS_ROOT=/var/www
+PROJECT=php_lib
 
 
 # update project
-echo Updating $PROJECT_NAME ...
+echo Updating $PROJECT ...
 
 USER=`cat ~/.cvs.shadow1`
 PASS=`cat ~/.cvs.shadow2`
@@ -18,24 +18,24 @@ else
 fi
 
 
-cd $PROJECT_ROOT
+cd $PROJECTS_ROOT
 
 export CVSROOT=:pserver:$USER:$PASS@localhost:$PORT/home/cvs
 cvs -d $CVSROOT login
 
-#cvs -d $CVSROOT -qr checkout -PR -d $PROJECT_NAME -r HEAD $PROJECT_NAME
-#-----------------------------------------------------------------------
-cvs -d $CVSROOT -qr update -CPRd -r HEAD $PROJECT_NAME
+#cvs -d $CVSROOT -qr checkout -PR -d $PROJECT -r HEAD $PROJECT
+#-------------------------------------------------------------
+cvs -d $CVSROOT -qr update -CPRd -r HEAD $PROJECT
 
 cvs -d $CVSROOT logout
 export -n CVSROOT
 
 
-find $PROJECT_NAME -follow -type f -print0 | xargs -0r chmod 0644
+find $PROJECT -follow -type f -print0 | xargs -0r chmod 0644
 
 # may take some time, let's do it in the background
-find $PROJECT_NAME -follow -type d \( ! -group apache -o ! -user apache \) ! -name 'CVS'   -print0 | xargs -0r chown apache:apache && \
-find $PROJECT_NAME -follow -type d                                                         -print0 | xargs -0r chmod 0755          && \
-find $PROJECT_NAME -follow -type f   -path '*/bin*' -prune -regex '.*\.\(pl\|php\|sh\)'    -print0 | xargs -0r chmod 0754          && \
-find $PROJECT_NAME -follow -type f ! -path '*/bin*' -prune ! -perm 0644                    -print0 | xargs -0r chmod 0644          && \
-find $PROJECT_NAME -follow -name '.#*'                                                     -print0 | xargs -0r rm                  &
+find $PROJECT -follow -type d \( ! -group apache -o ! -user apache \) ! -name 'CVS'   -print0 | xargs -0r chown apache:apache && \
+find $PROJECT -follow -type d                                                         -print0 | xargs -0r chmod 0755          && \
+find $PROJECT -follow -type f   -path '*/bin*' -prune -regex '.*\.\(pl\|php\|sh\)'    -print0 | xargs -0r chmod 0754          && \
+find $PROJECT -follow -type f ! -path '*/bin*' -prune ! -perm 0644                    -print0 | xargs -0r chmod 0644          && \
+find $PROJECT -follow -name '.#*'                                                     -print0 | xargs -0r rm                  &
