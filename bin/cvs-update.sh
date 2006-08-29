aa49573c0438e97aa2d07c12cebca5e916defe5a
Peter Walther <pewa-github@rosasurfer.com>
2006-08-29 12:45:14 +0000

    *** empty log message ***

--- bin/cvs-update.sh
+++ bin/cvs-update.sh
@@ -18,19 +18,19 @@ cd ${PROJECT_ROOT}
 export CVSROOT=:pserver:${USER}:${PASS}@localhost:${PORT}/home/cvs
 cvs -d ${CVSROOT} login
 
-#cvs -d ${CVSROOT} -qr checkout -PR -d php_lang -r HEAD php_lang
-#-------------------------------------------------------
-cvs -d ${CVSROOT} -qr update -CPRd -r HEAD php_lang
+#cvs -d ${CVSROOT} -qr checkout -PR -d php_lib -r HEAD php_lib
+#-------------------------------------------------------------
+cvs -d ${CVSROOT} -qr update -CPRd -r HEAD php_lib
 
 cvs -d ${CVSROOT} logout
 export -n CVSROOT
 
 
-find php_lang -follow -type f -print0 | xargs -0r chmod 0644
+find php_lib -follow -type f -print0 | xargs -0r chmod 0644
 
 # may take some time, let's do it in the background
-find php_lang -follow -type d \( ! -group apache -o ! -user apache \) ! -name 'CVS'   -print0 | xargs -0r chown apache:apache && \
-find php_lang -follow -type d                                                         -print0 | xargs -0r chmod 0755          && \
-find php_lang -follow -type f   -path '*/bin*' -prune -regex '.*\.\(pl\|php\|sh\)'    -print0 | xargs -0r chmod 0754          && \
-find php_lang -follow -type f ! -path '*/bin*' -prune ! -perm 0644                    -print0 | xargs -0r chmod 0644          && \
-find php_lang -follow -name '.#*'                                                     -print0 | xargs -0r rm                  &
+find php_lib -follow -type d \( ! -group apache -o ! -user apache \) ! -name 'CVS'   -print0 | xargs -0r chown apache:apache && \
+find php_lib -follow -type d                                                         -print0 | xargs -0r chmod 0755          && \
+find php_lib -follow -type f   -path '*/bin*' -prune -regex '.*\.\(pl\|php\|sh\)'    -print0 | xargs -0r chmod 0754          && \
+find php_lib -follow -type f ! -path '*/bin*' -prune ! -perm 0644                    -print0 | xargs -0r chmod 0644          && \
+find php_lib -follow -name '.#*'                                                     -print0 | xargs -0r rm                  &
