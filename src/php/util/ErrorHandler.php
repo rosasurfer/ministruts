d7e60e87a2e3e6a8e285db1bb10701d4479f15a6
Peter Walther <pewa-github@rosasurfer.com>
2006-12-07 00:16:15 +0000

    *** empty log message ***

--- src/php/util/ErrorHandler.php
+++ src/php/util/ErrorHandler.php
@@ -3,5 +3,27 @@
  * ErrorHandler
  */
 class ErrorHandler {
+
+
+   /**
+    * @param level -
+    * @param msg   -
+    * @param file  -
+    * @param line  -
+    * @param vars  -
+    *
+    * @return boolean - true, wenn der Fehler erfolgreich behandelt wurde
+    *                   false, wenn der Fehler weitergereicht werden soll, als wenn der ErrorHandler nicht registriert wäre
+    */
+   public static function handleError($level, $message, $file, $line, array $vars) {
+      return false;
+   }
+
+
+   /**
+    * @param exception - die zu behandelnde Exception
+    */
+   public static function handleException(Exception $exception) {
+   }
 }
 ?>
