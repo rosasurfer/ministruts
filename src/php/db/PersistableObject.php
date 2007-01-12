6d5d5f9179d5ae20ca226e73e1f9edb86d92fdfa
Peter Walther <pewa-github@rosasurfer.com>
2007-01-12 20:51:55 +0000

    *** empty log message ***

--- src/php/db/PersistableObject.php
+++ src/php/db/PersistableObject.php
@@ -7,6 +7,10 @@
 abstract class PersistableObject extends Object {
 
 
+   // 'leeres' Datenbankmapping
+   protected static $mappings;
+
+
    /**
     * Ob die aktuelle Instanz bereits gespeichert ist oder nicht.
     *
@@ -22,11 +26,39 @@ abstract class PersistableObject extends Object {
 
 
    /**
-    * Mappt Datenbankspalten auf Instanzvariablen.
+    * Mappt Datenbankspalten auf Instanzvariablen. Damit dies funktioniert, muß die statische
+    * Variable $mappings jeder persistenten Klasse deren Datenbankmapping enthalten.
+    *
+    * Beispiel:
+    * ---------
+    * class MyObject extends PersistableObject {
+    *
+    *    protected $id;
+    *    protected $created;
+    *    protected $key;
+    *    protected $name;
+    *                                     // Field names     => Property names
+    *                                     // ---------------------------------
+    *    protected static $mappings = array('a_id'          => 'id',
+    *                                       'creation_date' => 'created',
+    *                                       'object_key'    => 'key',
+    *                                       'fullname'      => 'name',
+    *    );
+    * }
     *
     * @param property - Name der zu mappenden Tabellenspalte
     * @param value    - Wert, auf den die entsprechende Variable der Klasseninstanz gesetzt werden soll
     */
-   abstract protected function __set($property, $value);
+   protected function __set($property, $value) {
+      static $mappings = null;
+      if ($mappings === null) {
+         eval('$mappings =& '.get_class($this).'::$mappings;');   // runtime evaluation of static property  (erst ab PHP6 fest eingebaut)
+      }
+
+      if (!isSet($mappings[$property]))
+         throw new RuntimeException("Database mapping for field '$property' not found (class ".get_Class($this).")");
+
+      $this->$mappings[$property] = $value;
+   }
 }
 ?>
