bd993da5144692542604ae23f0ad4fbae80c387c
Peter Walther <pewa-github@rosasurfer.com>
2007-10-01 18:39:40 +0000

    IP-Liste wird im Cache zwischengespeichert

--- src/php/util/TorHelper.php
+++ src/php/util/TorHelper.php
@@ -14,12 +14,11 @@ class TorHelper extends StaticFactory {
 
 
    /**
-    * Prüft, ob die übergebene IP-Adresse ein aktueller Exit-Node ist.
+    * Prüft, ob die übergebene IP-Adresse ein aktueller Tor-Exit-Node ist.
     *
     * @param string $ip - IP-Adresse
     *
-    * @return boolean   - TRUE, wenn die Adresse ein aktueller Exit-Node ist,
-    *                     FALSE andererseits
+    * @return boolean
     */
    public static function isExitNode($ip) {
       $nodes =& self:: getExitNodes();
@@ -28,14 +27,14 @@ class TorHelper extends StaticFactory {
 
 
    /**
-    * Gibt alle aktuellen Exit-Nodes zurück.
+    * Gibt die aktuellen Exit-Nodes zurück.
     *
     * @return array - assoziatives Array mit den IP-Adressen aller Exit-Nodes
     */
    private static function &getExitNodes() {
-      static $nodes = null;
+      $nodes = Cache ::get($key = 'tor_exit_nodes');
 
-      if ($nodes === null) {
+      if ($nodes == null) {
          $url = $handle = $response = $error = null;
          $size = sizeOf(self::$mirrors);
 
@@ -49,17 +48,17 @@ class TorHelper extends StaticFactory {
             $response = curl_exec($handle);
             if (!$response) {
                $error = CURL ::getError($handle);
-               //Logger  ::log('CURL error: '.$error.', url: '.$url, L_NOTICE, __CLASS__);
+               //Logger ::log('CURL error: '.$error.', url: '.$url, L_NOTICE, __CLASS__);
             }
          }
          curl_close($handle);
-
          if (!$response)
             throw new InfrastructureException('Could not retrieve Tor exit nodes, CURL error: '.$error.', url: '.$url);
 
          $nodes = array_flip(explode("\n", str_replace("\r\n", "\n", trim($response))));
-      }
 
+         Cache ::set($key, $nodes, 10 * MINUTE);
+      }
       return $nodes;
    }
 }
