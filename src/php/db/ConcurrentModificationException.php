5e8890b0ac20726d012505196836aab0ed119144
Peter Walther <pewa-github@rosasurfer.com>
2007-11-25 12:10:46 +0000

    *** empty log message ***

--- src/php/db/ConcurrentModificationException.php
+++ src/php/db/ConcurrentModificationException.php
@@ -3,6 +3,8 @@
  * ConcurrentModificationException
  *
  * Markiert Fehler beim gleichzeitigen Schreibzugriff auf PersistableObject-Instanzen.
+ *
+ * TODO: überflüssig, refaktorieren
  */
 class ConcurrentModificationException extends InfrastructureException {
 }
