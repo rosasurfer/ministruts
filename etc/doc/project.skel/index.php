fb9b9afe1fa7ec46d33b53995d7ff7a9c3c54a65
Peter Walther <pewa-github@rosasurfer.com>
2008-01-12 14:04:08 +0000

    *** empty log message ***

--- etc/doc/project.skel/index.php
+++ etc/doc/project.skel/index.php
@@ -3,8 +3,9 @@
  * Template for an index.php file
  * ------------------------------
  * Usually you don't need anything more in this file and yes, this directory is visible by the web.
- * It's the application's root directory.  You can virtually link it to whatever subdirectory you like.
- * (via WEB-INF/etc/httpd-conf/httpd-include.conf)
+ * It is the application's root directory.  You can virtually link it to whatever web directory you
+ * like, you may even nest one project into another (see "WEB-INF/etc/httpd/httpd-include.conf").
+ * Make sure to modify this file's line "define('APPLICATION_CONTEXT', '...')" appropriately.
  */
 
 include(dirName(__FILE__).'/WEB-INF/classes/classes.php');
