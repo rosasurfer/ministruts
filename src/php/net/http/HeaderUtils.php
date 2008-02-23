8eb15bdd24896c3d8db4e226ec936263a3b58fae
Peter Walther <pewa-github@rosasurfer.com>
2008-02-23 00:11:42 +0000

    *** empty log message ***

--- src/php/net/http/HeaderUtils.php
+++ src/php/net/http/HeaderUtils.php
@@ -13,6 +13,7 @@ final class HeaderUtils extends StaticClass {
    public static function redirect($url) {
       $request = Request ::me();
 
+      // TODO: Umleitung auf relative URL's funktioniert nicht: home "./" -> http://domain/content./
       if ($request->isSession()) {
          $session = $request->getSession();
          if ($session->isNew() || SID !== '') {   // bleiben wir innerhalb der Domain und Cookies sind aus, wird eine evt. Session-ID weitergegeben
@@ -25,13 +26,13 @@ final class HeaderUtils extends StaticClass {
          }
          session_write_close();
       }
+      // TODO: QueryString muß kodiert werden
+      header('Location: '.$url);
+      exit();                       // Ausgabe weiteren Contents verhindern
 
       /** TODO: HTTP/1.1 requires an absolute URI as argument to 'Location:' including the scheme, hostname and
        *        absolute path, but some clients accept relative URIs. You can usually use $_SERVER['HTTP_HOST'],
        *        $_SERVER['PHP_SELF'] and dirname() to make an absolute URI from a relative one yourself.
        */
-      // TODO: QueryString muß kodiert werden
-      header('Location: '.$url);
-      exit();                       // Ausgabe weiteren Contents verhindern
    }
 }
