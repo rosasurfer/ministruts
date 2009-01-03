2401314035708c39d0b60c488f860140d62b6845
Peter Walther <pewa-github@rosasurfer.com>
2009-01-03 17:21:03 +0000

    *** empty log message ***

--- src/php/locking/SystemFiveLock.php
+++ src/php/locking/SystemFiveLock.php
@@ -46,6 +46,8 @@ final class SystemFiveLock extends Lock {
     * Sorgt bei Zerstörung der Instanz dafür, daß ein evt. noch gehaltenes Lock freigegeben wird.
     */
    public function __destruct() {
+      echoPre(__METHOD__);
+
       $this->release();
    }
 
@@ -71,6 +73,8 @@ final class SystemFiveLock extends Lock {
       if ($this->isValid()) {
          sem_remove(self::$pool[$this->key]);
          unset(self::$pool[$this->key]);
+
+         echoPre(__METHOD__);
       }
    }
 }
