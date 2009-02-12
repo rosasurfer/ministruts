da955168f70cfb44a7d94f3f4e24d3ea4290eb7d
Peter Walther <pewa-github@rosasurfer.com>
2009-02-12 14:12:29 +0000

    Logging von unterdrückten Fehlern

--- src/php/locking/SystemFiveLock.php
+++ src/php/locking/SystemFiveLock.php
@@ -8,6 +8,10 @@
 final class SystemFiveLock extends BaseLock {
 
 
+   private static /*bool*/ $logDebug,
+                  /*bool*/ $logInfo,
+                  /*bool*/ $logNotice;
+
    private static /*Resource[]*/ $semIds;
 
    private /*string*/ $key;
@@ -36,6 +40,11 @@ final class SystemFiveLock extends BaseLock {
       if ($key !== (string)$key)      throw new IllegalTypeException('Illegal type of argument $key: '.getType($key));
       if (isSet(self::$semIds[$key])) throw new RuntimeException('Dead-lock detected: already holding a lock for key "'.$key.'"');
 
+      $loglevel        = Logger ::getLogLevel(__CLASS__);
+      self::$logDebug  = ($loglevel <= L_DEBUG );
+      self::$logInfo   = ($loglevel <= L_INFO  );
+      self::$logNotice = ($loglevel <= L_NOTICE);
+
       $decId = $this->getKeyId($key);
       $hexId = decHex($decId);
 
@@ -56,6 +65,8 @@ final class SystemFiveLock extends BaseLock {
                           || $ex->getMessage()=='sem_get(): failed acquiring SYSVSEM_SETVAL for key 0x'.$hexId.': Identifier removed'
                           || $ex->getMessage()=='sem_acquire(): failed to acquire key 0x'.$hexId.': Invalid argument'
                           || $ex->getMessage()=='sem_acquire(): failed to acquire key 0x'.$hexId.': Identifier removed')) {
+               self::$logInfo && Logger ::log($ex, L_INFO, __CLASS__);
+
                uSleep(100000); // Endlosschleife verhindern und 100 msec. warten
                continue;
             }
