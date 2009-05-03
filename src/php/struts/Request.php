ff194cbe16b50b9f794bc5772eea1200288e5e02
Peter Walther <pewa-github@rosasurfer.com>
2009-05-03 18:00:45 +0000

    Bugfix

--- src/php/struts/Request.php
+++ src/php/struts/Request.php
@@ -429,6 +429,9 @@ final class Request extends Singleton {
             $header = $_SERVER['HTTP_HTTP_X_UP_FORWARDED_FOR'];
          }
          elseif (isSet($_SERVER['HTTP_X_DDOSPROXY'])) {              // First-Colo Proxy
+            $header = $_SERVER['HTTP_X_DDOSPROXY'];
+         }
+         elseif (isSet($_SERVER['HTTP_HTTP_X_DDOSPROXY'])) {         // First-Colo Proxy
             $header = $_SERVER['HTTP_HTTP_X_DDOSPROXY'];
          }
          elseif (isSet($_SERVER[''])) {
