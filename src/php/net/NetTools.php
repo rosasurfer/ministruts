ab0c01f298062e8abd6f6a1148c0be15975fe2b9
Peter Walther <pewa-github@rosasurfer.com>
2009-05-04 20:43:33 +0000

    *** empty log message ***

--- src/php/net/NetTools.php
+++ src/php/net/NetTools.php
@@ -20,4 +20,41 @@ final class NetTools extends StaticClass {
 
       return getHostByAddr($address);
    }
+
+
+   /**
+    * Ob die IP-Adresse auf einen Proxy-Server weist.
+    *
+    * @param string $address - IP-Adresse
+    *
+    * @return boolean
+    */
+   public static function isProxyAddress($address) {
+      if (!is_string($address)) throw new IllegalTypeException('Illegal type of parameter $address: '.getType($address));
+      if (!strLen($address))    throw new InvalidArgumentException('Invalid argument $address: '.$address);
+
+      static $proxys = null;
+
+      if ($proxys === null) {
+         $proxys = array();
+
+         // Config einlesen
+         $value = Config ::get('proxys', null);
+         foreach (explode(',', $value) as $value) {
+            $value = trim($value);
+            if (strLen($value))
+               $proxys[$value] = $value;
+         }
+      }
+
+      echoPre($proxys);
+
+      if (isSet($proxys[$address]))
+         return true;
+
+      if (String ::endsWith(self ::getHostByAddr($address), '.proxy.aol.com', true))
+         return true;
+
+      return false;
+   }
 }
