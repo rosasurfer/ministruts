4a2998c1db3414991def5859a1b673ddf499cdf9
Peter Walther <pewa-github@rosasurfer.com>
2009-05-12 13:22:37 +0000

    *** empty log message ***

--- src/php/util/CommonValidator.php
+++ src/php/util/CommonValidator.php
@@ -81,7 +81,7 @@ class CommonValidator extends StaticClass {
          if ($bytes[0] == 127)                  // 127.0.0.0 - 127.255.255.255
             return false;
 
-         if ($bytes[0]==169)                    // 169.0.0.0 - 169.255.255.255 !!! ist wahrscheinlich niemandem zugewiesen
+         if ($bytes[0]==169)                    // 169.0.0.0 - 169.255.255.255 !!! wem zugewiesen? niemandem?
             return false;
 
          if ($bytes[0] == 172)                  // 172.16.0.0 - 172.31.255.255
@@ -89,7 +89,6 @@ class CommonValidator extends StaticClass {
 
          if ($bytes[0]==192)                    // 192.168.0.0 - 192.168.255.255
             return ($bytes[1]!=168);
-
       }
 
       // dieses TRUE ist eher spekulativ
