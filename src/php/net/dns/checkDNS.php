840017a94a87c1d0766d87ae0c703beae111826a
Peter Walther <pewa-github@rosasurfer.com>
2009-06-25 20:47:47 +0000

    Script zum automatischen Überprüfen von DNS-Einträgen

---
+++ src/php/net/dns/checkDNS.php
@@ -0,0 +1,97 @@
+#!/usr/bin/php -Cq
+<?
+set_time_limit(0);
+
+// MiniStruts einbinden
+require(dirName(__FILE__).'/../ministruts/src/phpLib.php');
+
+
+define('APPLICATION_NAME', 'DNS-Checker');
+
+
+
+/**
+ * Führt eine DNS-Abfrage durch und gibt den ermitelten Wert zurück
+ *
+ * @param $string $domain - Domain-Name, für den die Abfrage durchgeführt werden soll
+ * @param $string $type   - Typ des abzufragenden Wertes (A, MX, NS, TXT, etc.)
+ *
+ * @return string - Wert
+ */
+function queryDNS($domain, $type) {
+   $result = null;
+
+   switch ($type) {
+      case 'A':
+         $result = dns_get_record($domain, DNS_A);
+         $result = ($result && isSet($result[0]['ip'])) ? $result[0]['ip'] : null;
+         break;
+
+      case 'MX':
+         $result = dns_get_record($domain, DNS_MX);
+         $result = ($result && isSet($result[0]['target'])) ? $result[0]['target'] : null;
+         break;
+
+      case 'TXT'  :
+         $result = dns_get_record($domain, DNS_TXT);
+         $result = ($result && isSet($result[0]['txt'])) ? $result[0]['txt'] : null;
+         break;
+
+      case 'CNAME':
+      case 'HINFO':
+      case 'NS'   :
+      case 'PTR'  :
+      case 'SOA'  :
+      case 'AAAA' :
+      case 'SRV'  :
+      case 'NAPTR':
+      case 'A6'   :
+      case 'ALL'  :
+      case 'ANY'  :
+         break;
+
+      default:
+         throw new InvalidArgumentException('Invalid argument $type: '.$type);
+   }
+
+   return $result;
+}
+
+
+// DNS-Einträge
+$domains = Config::get('dns.domain');
+
+foreach ($domains as $domain => $domainValues) {
+   foreach ($domainValues as $type => $value) {
+      if ($type != 'subdomain') {
+         $result = queryDNS($domain, $type);
+         if ($result != $value) {
+            if ($type == 'TXT') {
+               $value  = '"'.$value.'"';
+               $result = '"'.$result.'"';
+            }
+            echoPre("DNS error detected for      $domain: required $type value: $value,   found: $result");
+         }
+         continue;
+      }
+
+      foreach ($value as $subdomain => $subdomainValues) {
+         foreach ($subdomainValues as $type => $value) {
+            $result = queryDNS("$subdomain.$domain", $type);
+            if ($result != $value)
+               echoPre('DNS error detected for '.str_pad($subdomain, 4, ' ', STR_PAD_LEFT).".$domain: required $type value: $value,   found: $result");
+         }
+      }
+   }
+}
+
+
+// Reverse-DNS-Einträge
+$ips = Config::get('dns.ip');
+
+foreach ($ips as $ip => $value) {
+   $result = getHostByAddr($ip);
+   if ($result != $value)
+      echoPre("Reverse DNS error detected for $ip: required PTR value: $value,   found: $result");
+}
+?>
