519ad5873c3accdaab0977612420882418e79ca3
Peter Walther <pewa-github@rosasurfer.com>
2010-12-18 13:56:34 +0000

    minor changes

--- src/php/util/Config.php
+++ src/php/util/Config.php
@@ -58,8 +58,7 @@
  * db.database  = db_test
  *
  * # Kommentar in eigener Zeile
- *
- * logger.Action = warn    # Kommentar innerhalb einer Zeile
+ * logger.Action = warn          # Kommentar am Ende einer Zeile
  */
 final class Config extends Object {
 
