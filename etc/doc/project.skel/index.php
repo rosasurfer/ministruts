17a956c8101f43ace14c22c5d3539af981b1fad3
Peter Walther <pewa-github@rosasurfer.com>
2011-11-15 23:11:19 +0000

    *** empty log message ***

--- etc/doc/project.skel/index.php
+++ etc/doc/project.skel/index.php
@@ -4,7 +4,7 @@
  * ------------------------------
  * Usually you don't need anything more in this file. This directory is visible by the web and it's
  * the application's root directory.  You can virtually link it to whatever web server directory
- * you like, you may even nest one project into another (see "WEB-INF/etc/httpd/httpd-include.conf").
+ * you like, you may even nest one project into another (see "WEB-INF/conf/httpd-include.conf").
  */
 
 include(dirName(__FILE__).'/WEB-INF/classes/classes.php');
