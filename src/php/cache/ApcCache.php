3857b8a7bfe7e5630f9f79081e165829adca0ffd
Peter Walther <pewa-github@rosasurfer.com>
2012-09-05 18:46:15 +0000

    *** empty log message ***

--- src/php/cache/ApcCache.php
+++ src/php/cache/ApcCache.php
@@ -139,18 +139,22 @@ final class ApcCache extends CachePeer {
       $data    = array($value, $dependency);
 
       /**
-       * PHP 5.3.3/APC 3.1.3p1
-       * ---------------------
-       * Bug 1: apc_add() und apc_store() geben FALSE zurück, wenn sie innerhalb eines Requests für denselben Key
-       *        mehrmals aufgerufen werden. Keine Lösung gefunden
+       * PHP 5.3.3/APC 3.1.3
+       * -------------------
+       * Bug 1: [apc-warning] Potential cache slam averted for key '...'
+       *        apc_add() und apc_store() geben FALSE zurück, wenn sie innerhalb eines Requests für denselben Key
+       *        mehrmals aufgerufen werden.
        *
-       * Bug 2: Apache error log is filled by "[apc-warning] Potential cache slam averted for key '...'"
        *        @see https://bugs.php.net/bug.php?id=58832
        *        @see http://stackoverflow.com/questions/4983370/php-apc-potential-cache-slam-averted-for-key
-       *        @see http://notmysock.org/blog/php/user-cache-timebomb.html
-       *        @see http://serverfault.com/questions/342295/apc-keeps-crashing
        *
+       *        Lösung für APC >= 3.1.7: re-introduced setting apc.slam_defense=0
+       *        keine Lösung für APC 3.1.3 gefunden
+       *
+       *
+       *        @see http://serverfault.com/questions/342295/apc-keeps-crashing
        *        @see http://stackoverflow.com/questions/1670034/why-would-apc-store-return-false
+       *        @see http://notmysock.org/blog/php/user-cache-timebomb.html
        */
 
 
@@ -163,16 +167,16 @@ final class ApcCache extends CachePeer {
 
 
       // Wert speichern:
-      // - möglichst apc_add() benutzen (minimiert "[apc-warning] Potential cache slam averted for key '...'")
-      // - wegen diverser Bugs keine APC-TTL setzen, die tatsächliche TTL wird in self::isCached() geprüft
+      // - möglichst apc_add() benutzen (minimiert Lock-Wait)
+      // - keine APC-TTL setzen, die tatsächliche TTL wird in self::isCached() geprüft (diverse APC-Bugs)
       if (function_exists('apc_add')) {                                          // APC >= 3.0.13
          if (!apc_add($fullKey, array($created, $expires, serialize($data)))) {
-            Logger ::log('apc_add() unexpectedly returned FALSE for $key "'.$fullKey.'" '.($isKey ? '(did exist and was deleted)':'(did not exist)'), L_WARN, __CLASS__);
+            //Logger ::log('apc_add() unexpectedly returned FALSE for $key "'.$fullKey.'" '.($isKey ? '(did exist and was deleted)':'(did not exist)'), L_WARN, __CLASS__);
             return false;
          }
       }
       else if (!apc_store($fullKey, array($created, $expires, serialize($data)))) {
-         Logger ::log('apc_store() unexpectedly returned FALSE for $key "'.$fullKey.'" '.($isKey ? '(did exist and was deleted)':'(did not exist)'), L_WARN, __CLASS__);
+         //Logger ::log('apc_store() unexpectedly returned FALSE for $key "'.$fullKey.'" '.($isKey ? '(did exist and was deleted)':'(did not exist)'), L_WARN, __CLASS__);
          return false;
       }
 
