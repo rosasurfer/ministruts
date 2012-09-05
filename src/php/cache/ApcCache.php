87d4cdbfd891bbf34803e9bdde294bb047518925
Peter Walther <pewa-github@rosasurfer.com>
2012-09-05 18:21:44 +0000

    *** empty log message ***

--- src/php/cache/ApcCache.php
+++ src/php/cache/ApcCache.php
@@ -141,14 +141,16 @@ final class ApcCache extends CachePeer {
       /**
        * PHP 5.3.3/APC 3.1.3p1
        * ---------------------
-       * Bug 1: apc_store() unexpectedly returned FALSE
-       *        @see http://stackoverflow.com/questions/1670034/why-would-apc-store-return-false
+       * Bug 1: apc_add() und apc_store() geben FALSE zurück, wenn sie innerhalb eines Requests für denselben Key
+       *        mehrmals aufgerufen werden. Keine Lösung gefunden
        *
        * Bug 2: Apache error log is filled by "[apc-warning] Potential cache slam averted for key '...'"
        *        @see https://bugs.php.net/bug.php?id=58832
        *        @see http://stackoverflow.com/questions/4983370/php-apc-potential-cache-slam-averted-for-key
        *        @see http://notmysock.org/blog/php/user-cache-timebomb.html
        *        @see http://serverfault.com/questions/342295/apc-keeps-crashing
+       *
+       *        @see http://stackoverflow.com/questions/1670034/why-would-apc-store-return-false
        */
 
 
@@ -156,49 +158,24 @@ final class ApcCache extends CachePeer {
       if (function_exists('apc_exists')) $isKey =        apc_exists($fullKey);   // APC >= 3.1.4
       else                               $isKey = (bool) apc_fetch ($fullKey);
 
-      if ($isKey) {
-         if (!apc_delete($fullKey))
-            Logger ::log('apc_delete() unexpectedly returned FALSE for key "'.$fullKey.'"', L_WARN, __CLASS__);
+      if ($isKey && !apc_delete($fullKey))
+         Logger ::log('apc_delete() unexpectedly returned FALSE for key "'.$fullKey.'"', L_WARN, __CLASS__);
 
-         if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-            echoPre(__METHOD__.'() deleted existing key "'.$fullKey.'" isKey(now) = '.(int)(bool)apc_fetch($fullKey));
-         }
-      }
-
-      if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-         echoPre(__METHOD__.'() storing key "'.$fullKey.'"');
-      }
 
       // Wert speichern:
       // - möglichst apc_add() benutzen (minimiert "[apc-warning] Potential cache slam averted for key '...'")
       // - wegen diverser Bugs keine APC-TTL setzen, die tatsächliche TTL wird in self::isCached() geprüft
       if (function_exists('apc_add')) {                                          // APC >= 3.0.13
          if (!apc_add($fullKey, array($created, $expires, serialize($data)))) {
-            $info = ($isKey ? ' (did exist and was deleted, ' : ' (did not exist, ')."new value: created=$created, expires=$expires, data=$data)";
+            $info = ($isKey ? ' (did exist and was deleted,' : ' (did not exist,')." new value: created=$created, expires=$expires, data=$data)";
             Logger ::log('apc_add() unexpectedly returned FALSE for $key "'.$fullKey.'"'.$info, L_WARN, __CLASS__);
-            if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-               echoPre(__METHOD__.'() trying again to set key "'.$fullKey.'"');
-            }
-            if (!apc_add($fullKey, array($created, $expires, serialize($data)))) {
-               if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-                  echoPre(__METHOD__.'() apc_add() failed again');
-               }
-               return false;
-            }
+            return false;
          }
       }
       else if (!apc_store($fullKey, array($created, $expires, serialize($data)))) {
-         $info = ($isKey ? ' (did exist and was deleted, ' : ' (did not exist, ')."new value: created=$created, expires=$expires, data=$data)";
+         $info = ($isKey ? ' (did exist and was deleted,' : ' (did not exist,')." new value: created=$created, expires=$expires, data=$data)";
          Logger ::log('apc_store() unexpectedly returned FALSE for $key "'.$fullKey.'"'.$info, L_WARN, __CLASS__);
-         if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-            echoPre(__METHOD__.'() trying again to set key "'.$fullKey.'"');
-         }
-         if (!apc_store($fullKey, array($created, $expires, serialize($data)))) {
-            if (isSet($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']=='127.0.0.1') {
-               echoPre(__METHOD__.'() apc_store() failed again');
-            }
-            return false;
-         }
+         return false;
       }
 
       $this->getReferencePool()->set($key, $value, $expires, $dependency);
