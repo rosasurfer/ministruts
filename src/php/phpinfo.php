e9813c50973bac790ffac57f705f5def1040d606
Peter Walther <pewa-github@rosasurfer.com>
2012-09-07 10:17:15 +0000

    *** empty log message ***

--- src/php/phpinfo.php
+++ src/php/phpinfo.php
@@ -121,6 +121,8 @@ if (!extension_loaded('apc'))                                                $is
 if ( extension_loaded('apc')) {
    if (phpVersion('apc') >= '3.1.3' && phpVersion('apc') < '3.1.7')          $isWarning = 1|echoPre('Warning: You are working with a buggy APC version (a version < 3.1.3 or >= 3.1.7 is needed)');
    if (!ini_get('apc.enabled'))                                              $isWarning = 1|echoPre('Warning: apc.enabled is not On');                   // warning "Potential cache slam averted for key '...'" http://bugs.php.net/bug.php?id=58832
+   if ( ini_get('apc.report_autofilter'))                                    $isWarning = 1|echoPre('Warning: apc.report_autofilter is not Off');
+
    if (WINDOWS) {       // Entwicklungsumgebung
       if      (ini_get('apc.stat'))                                          $isWarning = 1|echoPre('Warning: apc.stat is not Off');
       else if (ini_get('apc.cache_by_default'))                              $isWarning = 1|echoPre('Warning: apc.cache_by_default is not Off');         // "On" läßt manche Windows-APC-Versionen crashen (apc-error: cannot redeclare class ***)
@@ -129,7 +131,6 @@ if ( extension_loaded('apc')) {
       if (!ini_get('apc.cache_by_default'))                                  $isWarning = 1|echoPre('Warning: apc.cache_by_default is not On');
       if ( ini_get('apc.stat'))                                              $isWarning = 1|echoPre('Warning: apc.stat is not Off');                     // es soll gecacht werden
       if (!ini_get('apc.write_lock'))                                        $isWarning = 1|echoPre('Warning: apc.write_lock is not On');                // für beste Performance möglichst "Off" (Dateiänderungen sollen live nicht möglich sein)
-      if (!ini_get('apc.report_autofilter'))                                 $isWarning = 1|echoPre('Warning: apc.report_autofilter is not On');
 
       if (phpVersion('apc') >= '3.1.3' && phpVersion('apc') < '3.1.7') {
          if (ini_get('apc.include_once_override'))                           $isWarning = 1|echoPre('Warning: apc.include_once_override is not Off');
