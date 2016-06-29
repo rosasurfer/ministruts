a83663e8d3d74c797445c591c5b3fff854004870
Peter Walther <pewa-github@rosasurfer.com>
2016-06-29 14:30:48 +0300

    remove old Java WEB-INF skeleton

--- doc/project.skel/WEB-INF/classes/classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Definition of class to filename relations.
- */
-$dir = dirName(__FILE__).DIRECTORY_SEPARATOR;
-
-$__classes['HelloWorldAction'] = $dir.'actions/HelloWorldAction';
-
-unset($dir);
