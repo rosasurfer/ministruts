a5e6f77da55d5e302190e83b6565cbe6c308e269
Peter Walther <pewa-github@rosasurfer.com>
2016-07-01 13:14:30 +0300

    remove legacy incubator

--- src/incubator/net/dns/recordtypes/SPF_Record.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * A representation of a resource record of type <b>SPF</b>
- */
-class SPF_Record extends TXT_Record {
-   // There's no work to be done here, since the SPF record is identical to the TXT record - we
-   // just extend the TXT_Record record class and inherit everything.
-}
