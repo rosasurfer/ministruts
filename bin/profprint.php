05e1f09cedeb56747387d797565f36c6c72b762f
Peter Walther <pewa-github@rosasurfer.com>
2016-07-01 14:19:59 +0300

    remove legacy APD files

--- bin/profprint.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-define('APPLICATION_NAME', 'profprint');
-define('APPLICATION_ROOT',  dirName(__FILE__));
-
-// Library einbinden
-require(dirName(__FILE__).'/../src/load.php');
-include(dirName(__FILE__).'/../src/php/apd.php');
