9cb21391b41a6744f42de836bf0d28f1835e3100
Peter Walther <pewa-github@rosasurfer.com>
2016-07-09 23:53:22 +0300

    update deploy script

---
+++ bin/vcs-update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+# (1) change working directory
+SCRIPT_NAME=$(readlink -e "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_NAME")
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+cd "$PROJECT_DIR"
+
+
+# (2) update project
+[ ! -d ".git" ] && echo error: .git directory not found in project "$PROJECT_DIR" && exit
+echo Updating $(basename "$PROJECT_DIR")...
+
+git fetch origin                                                                  || exit
+git reset --hard origin/master                                                    || exit
+
+
+# (3) check/update additional requirements: dependencies, submodules, permissions
