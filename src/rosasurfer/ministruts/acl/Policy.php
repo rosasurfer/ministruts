83521a8588bf6e2102c8ca7b202083b3dc2af033
Peter Walther <pewa-github@rosasurfer.com>
2016-07-18 22:15:11 +0300

    rename IPolicy/IRole to PolicyInterface/RoleInterface

--- src/rosasurfer/ministruts/acl/Policy.php
+++ src/rosasurfer/ministruts/acl/Policy.php
@@ -8,5 +8,5 @@ use rosasurfer\ministruts\core\Object;
  * A Policy defines an access rule for a task (specific operation on a specific access-controlled resource).
  * If no custom Policy class is configured this class is the default implementation.
  */
-class Policy extends Object implements IPolicy {
+class Policy extends Object implements PolicyInterface {
 }
--- src/rosasurfer/ministruts/acl/Role.php
+++ src/rosasurfer/ministruts/acl/Role.php
@@ -8,5 +8,5 @@ use rosasurfer\ministruts\core\Object;
  * A Role organizes activity. It has a unique name and can hold a set of Policies and other Roles.
  * If no custom Role class is configured this class is the default implementation.
  */
-class Role extends Object implements IRole {
+class Role extends Object implements RoleInterface {
 }
