0c966de34f9a077036f2bf915ce6fd61b2bfdab7
Peter Walther <pewa-github@rosasurfer.com>
2016-07-20 22:05:22 +0300

    refine ..\config\ConfigInterface

--- src/rosasurfer/ministruts/config/ConfigInterface.php
+++ src/rosasurfer/ministruts/config/ConfigInterface.php
@@ -1,6 +1,28 @@
 <?php
 /**
- * Marker interface to implement by custom configuration classes.
+ * Interface to implement by custom configuration classes.
  */
 interface ConfigInterface {
+
+
+   /**
+    * Return the config setting with the specified key or the specified alternative value if no such is found.
+    *
+    * @param  string $key        - key
+    * @param  mixed  $onNotFound - alternative value
+    *
+    * @return mixed - config setting
+    *
+    * @throws RuntimeException - if no such setting is found and no alternative value was specified
+    */
+   public function get($key, $onNotFound=null);
+
+
+   /**
+    * Set the config setting with the specified key.
+    *
+    * @param  string $key   - key
+    * @param  string $value - new value
+    */
+   public function set($key, $value);
 }
