2c82893904c10b9b3095a28fa6c238ae4d4c9094
Peter Walther <pewa-github@rosasurfer.com>
2016-08-02 03:07:48 +0300

    update deploy script

--- bin/vcs-update.sh
+++ bin/vcs-update.sh
@@ -2,21 +2,28 @@
 #
 
 # (1) change working directory
-SCRIPT_NAME=$(readlink -e "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_NAME")
-PROJECT_DIR=$(dirname "$SCRIPT_DIR")
-cd "$PROJECT_DIR"
+SCRIPT=$(readlink -e "$0")
+DIR=$(dirname "$SCRIPT")
+
+while [ 1 ]; do
+   [ -d "$DIR/.git" ] && break
+   [ "$DIR" = "/"   ] && echo "error: .git directory not found" && exit 1
+   DIR=$(dirname "$DIR")
+done
+
+cd "$DIR"
 
 
 # (2) update project
-[ ! -d ".git" ] && echo error: .git directory not found in project "$PROJECT_DIR" && exit
-echo Updating $(basename "$PROJECT_DIR")...
+echo Updating "$DIR"...
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git fetch origin                                                                  || exit
-git status                                                                        || exit
-git diff --stat --ignore-space-at-eol HEAD                                        || exit
-git reset --hard origin/legacy-no-namespaces                                      || exit
-git status                                                                        || exit
+git --no-pager status                                                             || exit
+git --no-pager diff --stat --ignore-space-at-eol HEAD origin/$BRANCH              || exit
+git reset --hard origin/$BRANCH                                                   || exit
+git --no-pager status                                                             || exit
 
 
-# (3) check/update additional requirements: dependencies, submodules, permissions
+# (3) check/update additional requirements: dependencies, submodules, file permissions
