e2725c2bd0ff7866dfc38a3e81ff0d2a17dc944a
Peter Walther <pewa-github@rosasurfer.com>
2016-08-03 17:41:46 +0300

    skip empty forced mail receiver addresses

--- src/rosasurfer/net/mail/SMTPMailer.php
+++ src/rosasurfer/net/mail/SMTPMailer.php
@@ -193,7 +193,8 @@ class SMTPMailer extends Mailer {
       if (!$from) throw new InvalidArgumentException('Invalid argument $sender: '.$sender);
 
       if (!is_string($receiver)) throw new IllegalTypeException('Illegal type of parameter $receiver: '.getType($receiver));
-      $receiver = Config::getDefault()->get('mail.address.forced-receiver', $receiver);
+      $forced   = Config::getDefault()->get('mail.address.forced-receiver', null);
+      $receiver = $forced ?: $receiver;
       $to = $this->parseAddress($receiver);
       if (!$to) throw new InvalidArgumentException('Invalid argument $receiver: '.$receiver);
 
