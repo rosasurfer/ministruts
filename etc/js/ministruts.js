8e61843679f85e89b90f002491e2a6487c6d354e
Peter Walther <pewa-github@rosasurfer.com>
2016-08-03 17:45:44 +0300

    remove BOM header

--- etc/js/ministruts.js
+++ etc/js/ministruts.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Extend objects.
  */
 if (!String.prototype.capitalize     ) String.prototype.capitalize      = function()                  { return this.charAt(0).toUpperCase() + this.slice(1); }
