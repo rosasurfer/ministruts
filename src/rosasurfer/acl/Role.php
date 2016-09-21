26f2363b9e90a3880d92124f841ee91070213518
Peter Walther <pewa-github@rosasurfer.com>
2016-09-21 13:14:03 +0300

    minor

--- src/rosasurfer/acl/Role.php
+++ src/rosasurfer/acl/Role.php
@@ -5,8 +5,8 @@ use rosasurfer\core\Object;
 
 
 /**
- * A Role organizes activity. It has a unique name and can hold a set of Policies and other Roles.
- * If no custom Role class is configured this class is the default implementation.
+ * A <tt>Role</tt> organizes activity. It has a unique name and can hold a set of <tt>Policy</tt>s and other
+ * <tt>Role</tt>s. If no custom <tt>Role</tt> class is configured this class is the default implementation.
  */
 class Role extends Object implements RoleInterface {
 }
