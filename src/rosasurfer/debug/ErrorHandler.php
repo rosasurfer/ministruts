caec1bcfb248d7f500854396b003294bd6a75875
Peter Walther <pewa-github@rosasurfer.com>
2016-09-28 13:13:14 +0300

    draft for global ErrorHandler

--- src/rosasurfer/debug/ErrorHandler.php
+++ src/rosasurfer/debug/ErrorHandler.php
@@ -189,7 +189,7 @@ class ErrorHandler extends StaticClass {
     * Attempting to throw an exception from a destructor during script shutdown causes a fatal error. Therefore this
     * method has to be called manually from object destructors if an exception occurred. If the script is in the shutdown
     * phase the exception is passed on to the regular exception handler and the script is terminated. If the script is
-    * currently not in the shutdown phase this method ignores the exception. {@link http://php.net/manual/en/language.oop5.decon.php}
+    * currently not in the shutdown phase this method ignores the exception.
     *
     * @param  \Exception $exception
     *
