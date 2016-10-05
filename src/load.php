1791f63e3c7cb962782b80c6add58ebe5a8c80ca
Peter Walther <pewa-github@rosasurfer.com>
2016-10-05 20:11:12 +0300

    replace call_user_func() with closure

--- src/load.php
+++ src/load.php
@@ -21,14 +21,14 @@ include(MINISTRUTS_ROOT.'/src/rosasurfer/ministruts/helpers.php');
 /**
  * Register the framework's class loader.
  *
- * If the framework is used in a project not using Composer a class loader for the framework's classes is required.
+ * If the framework is used in a project not using Composer a class loader for the framework's classes is registered.
  * On the other hand if Composer is used this registration has no effect but is done anyway because detecting Composer
  * is not reliable and might fail in the future.
  *
  * The loader is registered after any other registered SPL loaders. To provide backward compatibility an existing
  * __autoload() function is registered first if no other SPL loader is yet registered.
  */
-call_user_func(function() {                                                   // encapsulated to protect global scope
+\Closure::bind(function() {                                             // prevent modifications of global scope
    // check the existing legacy auto-loader configuration
    $legacyAutoLoad = function_exists('__autoload');
    if ($legacyAutoLoad) {
@@ -55,4 +55,4 @@ call_user_func(function() {                                                   //
    if ($legacyAutoLoad && spl_autoload_functions()[0]!='__autoload') {
       spl_autoload_register('__autoload', $throw=true, $prepend=true);
    }
-});
+}, null)->__invoke();
