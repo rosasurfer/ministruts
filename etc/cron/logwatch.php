0cb9739a7971b7046e4dad89506c5537dc56ab85
Peter Walther <pewa-github@rosasurfer.com>
2016-10-27 14:35:20 +0300

    update logwatch script: new temp file name, fix she-bang

--- etc/cron/logwatch.php
+++ etc/cron/logwatch.php
@@ -1,4 +1,4 @@
-#!php
+#!/usr/bin/php
 <?php
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                                                    //
@@ -95,7 +95,7 @@ if (!is_writable($errorLog)) {             error('cannot access log file: '  .$e
 $errorLog = realPath($errorLog);
 
 // rename the file (we don't want to lock it, doing so could block the main app)
-$tempName = tempNam(dirName($errorLog), baseName($errorLog));
+$tempName = tempNam(dirName($errorLog), baseName($errorLog).'.');
 if (!rename($errorLog, $tempName)) {
    error('cannot rename log file: '  .$errorLog);
    exit(1);
