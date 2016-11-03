e5718aba944b67644ecc19e8db1e77d7ceb347f9
Peter Walther <pewa-github@rosasurfer.com>
2016-11-04 01:11:18 +0200

    typo: fix variable name

--- src/rosasurfer/util/Number.php
+++ src/rosasurfer/util/Number.php
@@ -17,17 +17,17 @@ class Number extends StaticClass {
     *
     * @param  int|double $value              - numeric value
     * @param  int        $decimals           - number of decimals  (default:   2)
-    * @param  string     $decimalSeparator   - decimal separator   (default: ".")
+    * @param  string     $decimalsSeparator  - decimals separator  (default: ".")
     * @param  string     $thousandsSeparator - thousands separator (default:  "")
     *
     * @return string
     */
-   public static function formatMoney($value, $decimals=2, $decimalSeparator='.', $thousandsSeparator='') {
+   public static function formatMoney($value, $decimals=2, $decimalsSeparator='.', $thousandsSeparator='') {
       if (!is_int($value) && !is_float($value)) throw new IllegalTypeException('Illegal type of parameter $value: '.getType($value));
       if (!is_int($decimals))                   throw new IllegalTypeException('Illegal type of parameter $decimals: '.getType($decimals));
       if (!is_string($decimalsSeparator))       throw new IllegalTypeException('Illegal type of parameter $decimalsSeparator: '.getType($decimalsSeparator));
       if (!is_string($thousandsSeparator))      throw new IllegalTypeException('Illegal type of parameter $thousandsSeparator: '.getType($thousandsSeparator));
 
-      return number_format($value, $decimals, $decimalSeparator, $thousandsSeparator);
+      return number_format($value, $decimals, $decimalsSeparator, $thousandsSeparator);
    }
 }
