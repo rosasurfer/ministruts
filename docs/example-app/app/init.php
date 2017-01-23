46fff744ca7a9dc56b198d04d74cb7cc4d3c5c5f
Peter Walther <pewa-github@rosasurfer.com>
2017-01-23 22:47:18 +0200

    add example project skeleton

---
+++ docs/example-app/app/init.php
@@ -0,0 +1,29 @@
+<?php
+if (PHP_VERSION_ID < 50600) exit('[FATAL] This application requires PHP >= 5.6');
+
+use rosasurfer\MiniStruts;
+use rosasurfer\util\PHP;
+
+
+// check app configuration
+!defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirname(__DIR__));
+!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'example-app');
+
+
+// load Composer
+require(APPLICATION_ROOT.'/etc/vendor/autoload.php');
+
+
+// global settings
+error_reporting(E_ALL);
+PHP::ini_set('error_log', APPLICATION_ROOT.'/etc/log/php-error.log');
+
+
+// initialize Ministruts
+$options = [
+   'config'            => APPLICATION_ROOT.'/app/config',
+   'handle-errors'     => MiniStruts::THROW_EXCEPTIONS,
+   'handle-exceptions' => true,
+   'global-helpers'    => true,
+];
+MiniStruts::init($options);
---
+++ docs/example-app/web/index.php
@@ -0,0 +1,12 @@
+<?php
+if (PHP_VERSION_ID < 50600) exit('[FATAL] This application requires PHP >= 5.6');
+
+use rosasurfer\ministruts\StrutsController;
+
+// configure and init the app
+!defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
+!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'example-app');
+require(APPLICATION_ROOT.'/app/init.php');
+
+// run the web app
+StrutsController::processRequest();
