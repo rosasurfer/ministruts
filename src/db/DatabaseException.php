b96b9ee66225e761fdd810c4f24682fe14b06a85
Peter Walther <pewa-github@rosasurfer.com>
2017-02-05 11:14:51 +0200

    move DatabaseException to package ./db

--- src/db/DatabaseException.php
+++ src/db/DatabaseException.php
@@ -1,9 +1,11 @@
 <?php
-namespace rosasurfer\exception;
+namespace rosasurfer\db;
+
+use rosasurfer\exception\RuntimeException;
 
 
 /**
  * DatabaseException
  */
-class DatabaseException extends InfrastructureException {
+class DatabaseException extends RuntimeException {
 }
