dd0c55a37446ec19495a04baa3ab87910e887904
Peter Walther <pewa-github@rosasurfer.com>
2017-02-28 11:44:59 +0200

    rename example application to "demo"

--- doc/demo/app/init.php
+++ doc/demo/app/init.php
@@ -5,7 +5,7 @@ use rosasurfer\util\PHP;
 
 // check app configuration
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirname(__DIR__));
-!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'example-app');
+!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'demo-ministruts');
 
 
 // load Composer
--- doc/demo/web/index.php
+++ doc/demo/web/index.php
@@ -1,7 +1,7 @@
 <?php
 // configure and init the app
 !defined('APPLICATION_ROOT') && define('APPLICATION_ROOT', dirName(__DIR__));
-!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'example');
+!defined('APPLICATION_ID'  ) && define('APPLICATION_ID',  'demo-ministruts');
 require(APPLICATION_ROOT.'/app/init.php');
 
 // run the web app
