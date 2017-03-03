559f532ade14d81536ab30104a85dccaa61ee73a
Peter Walther <pewa-github@rosasurfer.com>
2017-03-03 12:54:59 +0200

    add "/db/" package constants

---
+++ src/db/defines.php
@@ -0,0 +1,40 @@
+<?php
+namespace rosasurfer\db;
+
+
+/**
+ * Package constants
+ */
+
+
+// SQL serialization types (value representation in a SQL string)
+
+/**
+ * Bind type "boolean"
+ */
+const BIND_TYPE_BOOL = 1;
+
+/**
+ * Bind type "integer"
+ */
+const BIND_TYPE_INT = 2;
+
+/**
+ * Bind type "decimal"
+ */
+const BIND_TYPE_DECIMAL = 3;
+
+/**
+ * Bind type "string"
+ */
+const BIND_TYPE_STRING = 4;
+
+/**
+ * Bind type "blob"
+ */
+const BIND_TYPE_BLOB = 5;
+
+/**
+ * Bind type "array"
+ */
+//const BIND_TYPE_ARRAY = 6;        // TODO
