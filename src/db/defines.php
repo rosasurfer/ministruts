48028d6f2d30208941aa781203c30772b6da0640
Peter Walther <pewa-github@rosasurfer.com>
2017-03-03 22:33:00 +0200

    db: add column behavior constants

--- src/db/defines.php
+++ src/db/defines.php
@@ -5,7 +5,12 @@
 namespace rosasurfer\db;
 
 
-// SQL serialization types (value representation in a SQL string)
+// SQL serialization types (value representation in a SQL statement)
+
+/**
+ * Bind type "default". The bind type to use is derived from the property type.
+ */
+const BIND_TYPE_DEFAULT = 0;
 
 /**
  * Bind type "boolean"
@@ -34,5 +39,35 @@ const BIND_TYPE_BLOB = 5;
 
 /**
  * Bind type "array"
+ *
+const BIND_TYPE_ARRAY = 6;        // TODO
  */
-//const BIND_TYPE_ARRAY = 6;        // TODO
+
+/**
+ * Bind type "set"
+ *
+const BIND_TYPE_SET = 7;          // TODO
+ */
+
+
+// Column behaviour identifiers
+
+/**
+ * Identifier for a column being part of the primary key.
+ */
+const ID_PRIMARY = 1;
+
+/**
+ * Identifier for a column holding the "creation" time of the record.
+ */
+const ID_CREATE  = 2;
+
+/**
+ * Identifier for a column holding the "version" value of the record (typically the "last-updated" time).
+ */
+const ID_VERSION = 3;
+
+/**
+ * Identifier for a column holding the "soft-deletion" time of the record.
+ */
+const ID_DELETE  = 4;
