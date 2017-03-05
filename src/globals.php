e902b540feded7f3a9dd3ea0dca4a5f19757721c
Peter Walther <pewa-github@rosasurfer.com>
2017-03-05 17:52:46 +0200

    disregard

--- src/globals.php
+++ src/globals.php
@@ -462,7 +462,7 @@ function strToBool($value) {
 
 
 /**
- * Normalize line endings in a string. If the string contains mixed line endings the number of lines of the original
+ * Normalize line endings of a string. If the string contains mixed line endings the number of lines of the original
  * and the resulting string may differ. Netscape line endings are honored only if all line endings are Netscape format
  * (no mixed mode).
  *
