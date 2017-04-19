f6f7d21b044b0dea9e8f61f2d44301187691b7d5
Peter Walther <pewa-github@rosasurfer.com>
2017-04-19 15:52:07 +0300

    orm: implement DAO::refresh(PersistableObject) as PersistableObject::reload()

--- etc/phpstan/autoload.php
+++ etc/phpstan/autoload.php
@@ -4,7 +4,6 @@ include(__DIR__.'/../vendor/autoload.php');
 include(__DIR__.'/DynamicReturnType.php');
 include(__DIR__.'/DAO_FindAll_ReturnType.php');
 include(__DIR__.'/DAO_FindOne_ReturnType.php');
-include(__DIR__.'/DAO_Refresh_ReturnType.php');
-include(__DIR__.'/PersistableObject_CreateInstance_ReturnType.php');
+include(__DIR__.'/PersistableObject_PopulateNew_ReturnType.php');
 include(__DIR__.'/PersistableObject_Dao_ReturnType.php');
 include(__DIR__.'/Singleton_GetInstance_ReturnType.php');
