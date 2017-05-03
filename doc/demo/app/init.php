1f47cf67011c4f486f337b76ca65b3e05057d883
Peter Walther <pewa-github@rosasurfer.com>
2017-05-04 02:42:24 +0300

    minor

--- doc/demo/app/init.php
+++ doc/demo/app/init.php
@@ -1,16 +1,12 @@
 <?php
-use rosasurfer\MiniStruts;
-use rosasurfer\util\PHP;
-
+namespace rosasurfer;
 
 // load Composer
 require(__DIR__.'/../vendor/autoload.php');
 
-
 // global settings
 error_reporting(E_ALL);
-PHP::ini_set('error_log', __DIR__.'/../etc/log/php-error.log');
-
+ini_set('error_log', __DIR__.'/../etc/log/php-error.log');
 
 // initialize Ministruts
 MiniStruts::init([
