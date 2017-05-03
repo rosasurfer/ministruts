2a6020864a4c7c939e7dd998ea4d2ccbeb955d49
Peter Walther <pewa-github@rosasurfer.com>
2017-05-04 00:32:13 +0300

    improve error messages

--- bin/phpinfo.php
+++ bin/phpinfo.php
@@ -1,17 +1,9 @@
 #!/usr/bin/env php
 <?php
-namespace rosasurfer\bin\phpinfo;
-
-use rosasurfer\util\PHP;
-use function rosasurfer\echoPre;
-use const rosasurfer\NL;
-
-
 /**
  * Command line version of phpInfo()
  */
 require(__DIR__.'/../src/load.php');
 
-PHP::phpInfo();
-
-echoPre(NL.'loaded php.ini: "'.php_ini_loaded_file().'"');
+rosasurfer\util\PHP::phpInfo();
+echo PHP_EOL.'loaded php.ini: "'.php_ini_loaded_file().'"'.PHP_EOL;
