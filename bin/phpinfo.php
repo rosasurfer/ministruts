9f9ffb7a527fafad56e34fbaf1fe41f84d170930
Peter Walther <pewa-github@rosasurfer.com>
2017-08-07 17:30:13 +0300

    fix CLI phpinfo script

--- bin/phpinfo.php
+++ bin/phpinfo.php
@@ -3,7 +3,19 @@
 /**
  * Command line version of phpInfo()
  */
-require(__DIR__.'/../src/load.php');
+use rosasurfer\Application;
+use rosasurfer\util\PHP;
 
-rosasurfer\util\PHP::phpInfo();
+require(($dir=dirName(realPath(__FILE__))).'/../src/load.php');
+
+
+// php.ini settings
+error_reporting(E_ALL & ~E_DEPRECATED);
+PHP::ini_set('error_log', $dir.'/../php-error.log');
+
+
+$app = new Application();
+
+
+PHP::phpInfo();
 echo PHP_EOL.'loaded php.ini: "'.php_ini_loaded_file().'"'.PHP_EOL;
