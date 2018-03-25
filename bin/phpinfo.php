752262cab7b5cb05a9959fea75159baccc886eb8
Peter Walther <pewa-github@rosasurfer.com>
2018-03-25 19:27:13 +0300

    update CLI phpinfo()

--- bin/phpinfo.php
+++ bin/phpinfo.php
@@ -15,8 +15,7 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 PHP::ini_set('error_log', $root.'/php-error.log');
 
 
-$app = new Application();
-
+$app = new Application();                               // creating an application loads the configuration
 
 PHP::phpInfo();
 echo PHP_EOL.'loaded php.ini: "'.php_ini_loaded_file().'"'.PHP_EOL;
