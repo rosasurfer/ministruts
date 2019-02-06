14357f89e5e104ed84860e0c4a16bf66975634af
Peter Walther <pewa-github@rosasurfer.com>
2019-02-06 22:31:21 +0200

    add stub of FileSystem helper

---
+++ src/file/FileSystem.php
@@ -0,0 +1,13 @@
+<?php
+namespace rosasurfer\file;
+
+use rosasurfer\core\StaticClass;
+
+
+/**
+ * FileSystem
+ *
+ * File system related functionality
+ */
+class FileSystem extends StaticClass {
+}
