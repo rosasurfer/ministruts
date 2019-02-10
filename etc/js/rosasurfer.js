3acb4e32f3a3c05560179590d452e50e59cc5073
Peter Walther <pewa-github@rosasurfer.com>
2019-02-10 21:46:13 +0200

    fix broken JavaScript helpers

--- etc/js/rosasurfer.js
+++ etc/js/rosasurfer.js
@@ -281,7 +281,7 @@ var rosasurfer = {
      *
      * @return string
      */
-    gettype: function gettype(arg) {
+    getType: function getType(arg) {
         var type = typeof(arg);
         if (type == 'object') {
             if      (arg === null)    type = 'null';
@@ -308,9 +308,9 @@ var rosasurfer = {
     showProperties: function showProperties(arg, sort) {
         if (arg === undefined)                            return alert('rosasurfer.showProperties()\n\nPassed parameter: undefined');
         if (arg === null)                                 return alert('rosasurfer.showProperties()\n\nPassed parameter: null');
-        if (this.gettype(arg).startsWith('XrayWrapper ')) return this.showProperties(arg.wrappedJSObject, sort);
+        if (this.getType(arg).startsWith('XrayWrapper ')) return this.showProperties(arg.wrappedJSObject, sort);
 
-        var property='', properties=[], type=this.gettype(arg);
+        var property='', properties=[], type=this.getType(arg);
         sort = (sort===undefined) ? (type!='Array') : Boolean(sort);
         
         for (var i in arg) {
@@ -395,7 +395,7 @@ var rosasurfer = {
      * @param  mixed msg
      */
     logStatus: function logStatus(msg) {
-        if (this.gettype(msg) == 'Event') this.logEvent(msg);
+        if (this.getType(msg) == 'Event') this.logEvent(msg);
         else                              self.status = msg;
     },
 
