03ca840a0ec8c87936db9276ba083970d44cbe12
Peter Walther <pewa-github@rosasurfer.com>
2019-02-27 22:28:35 +0200

    add Input::isCommand() and Input::getCommand()

--- src/console/Input.php
+++ src/console/Input.php
@@ -37,9 +37,40 @@ class Input extends Object {
 
 
     /**
-     * Whether the argument with the given name is defined. Not whether the argument was specified..
+     * Whether the command with the given name is defined (not whether the command was specified). Valid commands consists
+     * of only lower-case letters. The same command may be defined multiple times.
      *
-     * @param  string $name - argument name: either all-uppercase or enclosed in angular brackets
+     * @param  string $name
+     *
+     * @return bool
+     */
+    public function isCommand($name) {
+        if (!($len=strlen($name)) || !isset($this->docoptResult[$name]))
+            return false;
+        return preg_match('/^[a-z]+$/', $name);
+    }
+
+
+    /**
+     * Whether the command with the given name was specified. Returns a non-zero value only if the command is defined.
+     * See {@link Input::isCommand()} for the definition of "command".
+     *
+     * @param  string $name
+     *
+     * @return int - number of times the command was specified; 0 (zero) if the command is not defined
+     */
+    public function getCommand($name) {
+        if ($this->isCommand($name))
+            return (int) $this->docoptResult[$name];
+        return 0;
+    }
+
+
+    /**
+     * Whether the argument with the given name is defined (not whether the argument was specified).
+     * Arguments are command line parameters defined either in angular brackets or in all-uppercase letters.
+     *
+     * @param  string $name - argument name
      *
      * @return bool
      */
@@ -49,17 +80,18 @@ class Input extends Object {
         if (!($len=strlen($name)) || !isset($this->docoptResult[$name]))
             return false;
 
-        $bracketed = ('<'==$name[0] && $name[$len-1]=='>');
-        $upperCase = ($name == strtoupper($name));
+        $isBracketed = ('<'==$name[0] && $name[$len-1]=='>');
+        $isUpperCase = ($name == strtoupper($name));
 
-        return ($bracketed || $upperCase);
+        return ($isBracketed || $isUpperCase);
     }
 
 
     /**
      * Return the single-value argument with the given name.
+     * See {@link Input::isArgument()} for the definition of "argument".
      *
-     * @param  string $name - either wrapped in angular brackets or all-uppercase name
+     * @param  string $name
      *
      * @return string|null - the argument value or NULL if the argument was not specified
      */
@@ -75,8 +107,9 @@ class Input extends Object {
 
     /**
      * Return the multi-value argument with the given name.
+     * See {@link Input::isArgument()} for the definition of "argument".
      *
-     * @param  string $name - either wrapped in angular brackets or all-uppercase name
+     * @param  string $name
      *
      * @return string[]|null - the argument values or NULL if the arguments were not specified
      */
@@ -91,9 +124,12 @@ class Input extends Object {
 
 
     /**
-     * Whether the option with the given name is defined. Not whether the option was specified..
+     * Whether the option with the given name is defined (not whether the option was specified).
+     * Options are command line parameters defined with one leading dash (short options) or with two leading dashes (long
+     * options). If an option can be specified in both ways the input values always reflect the long option and not the
+     * short one.
      *
-     * @param  string $name - option name: either a long "--" (precedence) or a short "-" option name (including dashes)
+     * @param  string $name - option name with leading dash(es)
      *
      * @return bool
      */
@@ -107,9 +143,10 @@ class Input extends Object {
 
 
     /**
-     * Return the single-value option with the given name. The value may be the defined default value.
+     * Return the single-value option with the given name (the returned value may be the defined default value).
+     * See {@link Input::isOption()} for the definition of "option".
      *
-     * @param  string $name - long (if defined) or short option name including leading dashes
+     * @param  string $name
      *
      * @return bool|string - the option value or FALSE if the option was not specified
      */
@@ -124,9 +161,10 @@ class Input extends Object {
 
 
     /**
-     * Return the multi-value option with the given name. The values may be the defined default values.
+     * Return the multi-value option with the given name (the returned values may be the defined default values).
+     * See {@link Input::isOption()} for the definition of "option".
      *
-     * @param  string $name - long (if defined) or short option name including leading dashes
+     * @param  string $name
      *
      * @return bool|string[] - the option value or FALSE if the options were not specified
      */
