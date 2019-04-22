12d18229adf9d11ade2508f9361db116528ef27f
Peter Walther <pewa-github@rosasurfer.com>
2019-04-22 20:18:39 +0300

    Output::error() handles mixed parameter types

--- src/console/io/Output.php
+++ src/console/io/Output.php
@@ -2,10 +2,10 @@
 namespace rosasurfer\console\io;
 
 use rosasurfer\core\Object;
-use rosasurfer\exception\IllegalTypeException;
+
+use function rosasurfer\printPretty;
 
 use const rosasurfer\CLI;
-use const rosasurfer\NL;
 
 
 /**
@@ -20,44 +20,23 @@ class Output extends Object {
      * @param  mixed $message
      */
     public function out($message) {
-        $var = $message;
-        if (is_object($var) && method_exists($var, '__toString') && !$var instanceof \SimpleXMLElement) {
-            $str = (string) $var;
-        }
-        elseif (is_object($var) || is_array($var)) {
-            $str = print_r($var, true);
-        }
-        elseif ($var === null) {
-            $str = '(null)';                                // analogous to typeof(null) = 'NULL'
-        }
-        elseif (is_bool($var)) {
-            $str = ($var ? 'true':'false').' (bool)';
-        }
-        else {
-            $str = (string) $var;
-        }
-
-        $len = strlen($message);
-        if ($len && $message[$len-1]!=NL)
-            $message .= NL;
+        $message = printPretty($message, $return=true);
 
         $hStream = CLI ? \STDOUT : fopen('php://stdout', 'a');
         fwrite($hStream, $message);
         if (!CLI) fclose($hStream);
+
+        echoPre($var);
     }
 
 
     /**
      * Write a message to STDERR.
      *
-     * @param  string $message
+     * @param  mixed $message
      */
     public function error($message) {
-        if (!is_string($message)) throw new IllegalTypeException('Illegal type of parameter $message: '.gettype($message));
-
-        $len = strlen($message);
-        if ($len && $message[$len-1]!=NL)
-            $message .= NL;
+        $message = printPretty($message, $return=true);
 
         $hStream = CLI ? \STDERR : fopen('php://stderr', 'a');
         fwrite($hStream, $message);
