cfd508d9b4a9b6370294f3b2d94e1d8c21141070
Peter Walther <pewa-github@rosasurfer.com>
2019-06-12 15:00:33 +0300

    add drop-in replacement for PHP function copy()

--- src/file/FileSystem.php
+++ src/file/FileSystem.php
@@ -15,8 +15,10 @@ class FileSystem extends StaticClass {
 
 
     /**
+     * Drop-in replacement for the built-in PHP function {@link \mkdir()}.
+     *
      * Make sure a directory exists. If the directory does not exist try to create it. By default this method will create
-     * specified sub-directories. It will set default permissions a bit more restrictive and will *not* emit a warning if a
+     * all specified sub-directories. It will set default permissions a bit more restrictive and will *not* emit a warning if a
      * directory already exists.
      *
      * @param  string   $path                 - directory name                                                              <br>
@@ -37,7 +39,7 @@ class FileSystem extends StaticClass {
 
             $ex = null;
             try {
-                \mkdir(...$args);                       // unpack new arguments as mkdir() will not accept $context = null
+                return \mkdir(...$args);                // unpack arguments as mkdir() will not accept $context = null
             }
             catch (IRosasurferException $ex) {}
             catch (\Throwable           $ex) { $ex = new RuntimeException($ex->getMessage(), $ex->getCode(), $ex); }
@@ -46,5 +48,25 @@ class FileSystem extends StaticClass {
             if ($ex) throw $ex->addMessage('Cannot create directory "'.$path.'"');
         }
         return true;
+    }
+
+
+    /**
+     * Drop-in replacement for the built-in PHP function {@link \copy()}.
+     *
+     * Copies a file. If the target directory does not exist try to create it. If the destination file already exists,
+     * it will be overwritten.
+     *
+     * @param  string   $source             - path to the source file
+     * @param  int      $destination        - destination file
+     * @param  resource $context [optional]
+     *
+     * @return bool - success status
+     */
+    public static function copy($source, $destination, $context = null) {
+        $dir = dirname($destination);
+        !is_dir($dir) && static::mkDir($dir);
+
+        return copy(...func_get_args());                // unpack arguments as copy() will not accept $context = null
     }
 }
