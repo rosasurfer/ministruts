c5a4f9142211cfa1275cfdf5b2173c1ba3733ffb
Peter Walther <pewa-github@rosasurfer.com>
2022-09-25 16:08:28 +0300

    check PHP requirements and prevent multiple includes

---
+++ src/load.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Protective wrapper around the framework.
+ */
+if (PHP_VERSION_ID < 50600 || PHP_VERSION_ID >= 60000) {
+    echo 'Error: unsupported PHP version '.PHP_VERSION.' (this release of "rosasurfer/ministruts" requires PHP 5.6)'.PHP_EOL;
+    exit(1);
+}
+
+// prevent multiple includes
+if (defined('rosasurfer\MINISTRUTS_ROOT')) return;
+
+// now include the framework
+require(__DIR__.'/bootstrap.php');
