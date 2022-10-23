1121387db5f0a164a21ba80c1d7937ed36562b9b
Peter Walther <pewa-github@rosasurfer.com>
2022-10-23 23:31:33 +0300

    update PHP requirements

--- src/load.php
+++ src/load.php
@@ -2,8 +2,8 @@
 /**
  * Protective wrapper around the framework.
  */
-if (PHP_VERSION_ID < 50600 || PHP_VERSION_ID >= 60000) {
-    echo 'Error: unsupported PHP version '.PHP_VERSION.' (this "rosasurfer/ministruts" version requires PHP 5.6)'.PHP_EOL;
+if (PHP_VERSION_ID < 50600 || PHP_VERSION_ID >= 70200) {
+    echo 'Error: unsupported PHP version '.PHP_VERSION.' (this "rosasurfer/ministruts" version requires PHP 5.6 to PHP 7.1)'.PHP_EOL;
     exit(1);
 }
 
