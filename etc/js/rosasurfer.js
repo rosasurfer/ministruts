6dec33557042e8bee29028f432ee181837507740
Peter Walther <pewa-github@rosasurfer.com>
2022-10-23 20:51:34 +0300

    update JavaScript helpers

--- etc/js/rosasurfer.js
+++ etc/js/rosasurfer.js
@@ -22,43 +22,40 @@ if (!Array.from) { Array.from = (function() {
     };
 
     // The length property of the from method is 1.
-    return function from(arrayLike/*, mapFn, thisArg */) {
-        // 1. Let C be the this value.
+    return function from(arrayLike/*, mapFn, thisArg*/) {
+        // 1) Let C be the this value.
         var C = this;
-        // 2. Let items be ToObject(arrayLike).
+        // 2) Let items be ToObject(arrayLike).
         var items = Object(arrayLike);
-        // 3. ReturnIfAbrupt(items).
+        // 3) ReturnIfAbrupt(items).
         if (arrayLike == null) throw new TypeError('Array.from requires an array-like object - not null or undefined');
-        // 4. If mapfn is undefined, then let mapping be false.
+        // 4) If mapfn is undefined, then let mapping be false.
         var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
         var T;
         if (typeof(mapFn) !== 'undefined') {
-            // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+            // 5a) If isCallable(mapfn) is false, throw a TypeError exception.
             if (!isCallable(mapFn)) throw new TypeError('Array.from: when provided, the second argument must be a function');
-            // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            if (arguments.length > 2)
-                T = arguments[2];
+            // 5b) If thisArg was supplied, let T be thisArg; else let T be undefined.
+            if (arguments.length > 2) T = arguments[2];
         }
-        // 10. Let lenValue be Get(items, "length").
-        // 11. Let len be ToLength(lenValue).
+        // 6) Let len be toLength(lenValue).
         var len = toLength(items.length);
-        // 13. If IsConstructor(C) is true, then
-        // 13. a. Let A be the result of calling the [[Construct]] internal method 
-        // of C with an argument list containing the single item len.
-        // 14. a. Else, Let A be ArrayCreate(len).
+        // 7) If isCallable(C) is true, then
+        // 7a) Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
+        // 7b) Else, Let A be ArrayCreate(len).
         var A = isCallable(C) ? Object(new C(len)) : new Array(len);
-        // 16. Let k be 0.
+        // 8) Let k be 0.
         var k = 0, kValue;
-        // 17. Repeat, while k < len… (also steps a - h)
+        // 9) Repeat, while k < len (also steps a - h)
         while (k < len) {
             kValue = items[k];
             if (mapFn) A[k] = typeof(T)==='undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
             else       A[k] = kValue;
             k++;
         }
-        // 18. Let putStatus be Put(A, "length", len, true).
+        // 10) Let putStatus be Put(A, "length", len, true).
         A.length = len;
-        // 20. Return A.
+        // 11) Return A.
         return A;
     };
   }());
@@ -76,23 +73,23 @@ if (!String.prototype.capitalizeWords) String.prototype.capitalizeWords = functi
 if (!String.prototype.decodeEntities ) String.prototype.decodeEntities  = function decodeEntities ()                                    { if (!String.prototype.decodeEntities.textarea) /*static*/ String.prototype.decodeEntities.textarea = document.createElement('textarea'); String.prototype.decodeEntities.textarea.innerHTML = this; return String.prototype.decodeEntities.textarea.value; }
 if (!String.prototype.startsWith     ) String.prototype.startsWith      = function startsWith     (/*string*/ prefix, /*int*/ pos)      { return this.substr(!pos || pos < 0 ? 0 : +pos, prefix.length) === prefix; }
 if (!String.prototype.endsWith       ) String.prototype.endsWith        = function endsWith       (/*string*/ suffix, /*int*/ this_len) { if (this_len===undefined || this_len > this.length) this_len = this.length; return this.substring(this_len - suffix.length, this_len) === suffix; }
-if (!String.prototype.includes       ) String.prototype.includes        = function includes       (/*string*/ string, /*int*/ start)    { if (typeof(start) !== 'number') start = 0; if (start + string.length > this.length) return false; return this.indexOf(string, start) !== -1; } 
+if (!String.prototype.includes       ) String.prototype.includes        = function includes       (/*string*/ string, /*int*/ start)    { if (typeof(start) !== 'number') start = 0; if (start + string.length > this.length) return false; return this.indexOf(string, start) !== -1; }
 if (!String.prototype.contains       ) String.prototype.contains        = String.prototype.includes;
 if (!String.prototype.trim           ) String.prototype.trim            = function trim           ()                                    { return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, ''); }
 if (!String.prototype.repeat         ) String.prototype.repeat          = function repeat         (/*int*/ count) {
     var str = ''+ this;
     count = +count;
-    if (count != count) 
+    if (count != count)
         count = 0;
     if (count < 0)         throw new RangeError('repeat count must be non-negative');
     if (count == Infinity) throw new RangeError('repeat count must be less than infinity');
     count = Math.floor(count);
-    if (!str.length || !count) 
+    if (!str.length || !count)
         return '';
     if (str.length * count >= 1 << 28) throw new RangeError('repeat count must not overflow maximum string size');
     var maxCount = str.length * count;
     count = Math.floor(Math.log(count) / Math.log(2));
-    while (count--) 
+    while (count--)
        str += str;
     str += str.substring(0, maxCount - str.length);
     return str;
@@ -218,7 +215,7 @@ var rosasurfer = {
 
 
     /**
-     * Whether or not a parameter exists in the query string.
+     * Whether a parameter exists in the query string.
      *
      * @param  string name           - parameter name
      * @param  string url [optional] - URL to check for the query parameter (default: the current page location)
@@ -287,7 +284,7 @@ var rosasurfer = {
             if      (arg === null)    type = 'null';
             else if (arg.constructor) type = arg.constructor.name || arg.constructor.toString();
             else                      type = ''+ arg;
-            
+
             if (type.startsWith('[object ')) {              // [object HTMLAnchorElement]
                 type = type.slice(8, -1);
             }
@@ -298,12 +295,66 @@ var rosasurfer = {
         return type;
     },
 
-
+    
+    /**
+     * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message Digest Algorithm, as defined in RFC 1321.
+     * Based on the original version of (c) Paul Johnston
+     *
+     * @param  string input
+     *
+     * @return string
+     */
+    md5: function md5(input) {
+        var hc = '0123456789abcdef';
+        function rh(n)                   { var j, s=''; for (j=0; j<=3; j++) s += hc.charAt((n>>(j*8+4)) & 0x0F) + hc.charAt((n>>(j*8)) & 0x0F); return s; }
+        function ad(x, y)                { var l=(x & 0xFFFF) + (y & 0xFFFF); var m=(x>>16) + (y>>16) + (l>>16); return (m<<16) | (l & 0xFFFF); }
+        function rl(n, c)                { return (n<<c) | (n>>>(32-c)); }
+        function cm(q, a, b,    x, s, t) { return ad(rl(ad(ad(a, q), ad(x, t)), s), b); }
+        function ff(a, b, c, d, x, s, t) { return cm((b&c) | ((~b) & d), a, b, x, s, t); }
+        function gg(a, b, c, d, x, s, t) { return cm((b&d) | (c & (~d)), a, b, x, s, t); }
+        function hh(a, b, c, d, x, s, t) { return cm(b^c^d, a, b, x, s, t); }
+        function ii(a, b, c, d, x, s, t) { return cm(c^(b|(~d)), a, b, x, s, t); }
+        function sb(x) {
+            var i, nblk=((x.length+8)>>6) + 1, blks=new Array(nblk*16); for (i=0; i<nblk*16; i++) blks[i]=0;
+            for (i=0; i<x.length; i++) blks[i>>2] |= x.charCodeAt(i) << ((i%4)*8);
+            blks[i>>2] |= 0x80<<((i%4)*8); blks[nblk*16-2] = x.length*8; return blks;
+        }
+        var i, x=sb(input), a=1732584193, b=-271733879, c=-1732584194, d=271733878, olda, oldb, oldc, oldd;
+        for (i=0; i<x.length; i+=16) {
+            olda = a; oldb = b; oldc = c; oldd = d;
+            a = ff(a,b,c,d,x[i+ 0], 7, -680876936); d = ff(d,a,b,c,x[i+ 1],12, -389564586); c = ff(c,d,a,b,x[i+ 2],17,  606105819);
+            b = ff(b,c,d,a,x[i+ 3],22,-1044525330); a = ff(a,b,c,d,x[i+ 4], 7, -176418897); d = ff(d,a,b,c,x[i+ 5],12, 1200080426);
+            c = ff(c,d,a,b,x[i+ 6],17,-1473231341); b = ff(b,c,d,a,x[i+ 7],22,  -45705983); a = ff(a,b,c,d,x[i+ 8], 7, 1770035416);
+            d = ff(d,a,b,c,x[i+ 9],12,-1958414417); c = ff(c,d,a,b,x[i+10],17,     -42063); b = ff(b,c,d,a,x[i+11],22,-1990404162);
+            a = ff(a,b,c,d,x[i+12], 7, 1804603682); d = ff(d,a,b,c,x[i+13],12,  -40341101); c = ff(c,d,a,b,x[i+14],17,-1502002290);
+            b = ff(b,c,d,a,x[i+15],22, 1236535329); a = gg(a,b,c,d,x[i+ 1], 5, -165796510); d = gg(d,a,b,c,x[i+ 6], 9,-1069501632);
+            c = gg(c,d,a,b,x[i+11],14,  643717713); b = gg(b,c,d,a,x[i+ 0],20, -373897302); a = gg(a,b,c,d,x[i+ 5], 5, -701558691);
+            d = gg(d,a,b,c,x[i+10], 9,   38016083); c = gg(c,d,a,b,x[i+15],14, -660478335); b = gg(b,c,d,a,x[i+ 4],20, -405537848);
+            a = gg(a,b,c,d,x[i+ 9], 5,  568446438); d = gg(d,a,b,c,x[i+14], 9,-1019803690); c = gg(c,d,a,b,x[i+ 3],14, -187363961);
+            b = gg(b,c,d,a,x[i+ 8],20, 1163531501); a = gg(a,b,c,d,x[i+13], 5,-1444681467); d = gg(d,a,b,c,x[i+ 2], 9,  -51403784);
+            c = gg(c,d,a,b,x[i+ 7],14, 1735328473); b = gg(b,c,d,a,x[i+12],20,-1926607734); a = hh(a,b,c,d,x[i+ 5], 4,    -378558);
+            d = hh(d,a,b,c,x[i+ 8],11,-2022574463); c = hh(c,d,a,b,x[i+11],16, 1839030562); b = hh(b,c,d,a,x[i+14],23,  -35309556);
+            a = hh(a,b,c,d,x[i+ 1], 4,-1530992060); d = hh(d,a,b,c,x[i+ 4],11, 1272893353); c = hh(c,d,a,b,x[i+ 7],16, -155497632);
+            b = hh(b,c,d,a,x[i+10],23,-1094730640); a = hh(a,b,c,d,x[i+13], 4,  681279174); d = hh(d,a,b,c,x[i+ 0],11, -358537222);
+            c = hh(c,d,a,b,x[i+ 3],16, -722521979); b = hh(b,c,d,a,x[i+ 6],23,   76029189); a = hh(a,b,c,d,x[i+ 9], 4, -640364487);
+            d = hh(d,a,b,c,x[i+12],11, -421815835); c = hh(c,d,a,b,x[i+15],16,  530742520); b = hh(b,c,d,a,x[i+ 2],23, -995338651);
+            a = ii(a,b,c,d,x[i+ 0], 6, -198630844); d = ii(d,a,b,c,x[i+ 7],10, 1126891415); c = ii(c,d,a,b,x[i+14],15,-1416354905);
+            b = ii(b,c,d,a,x[i+ 5],21,  -57434055); a = ii(a,b,c,d,x[i+12], 6, 1700485571); d = ii(d,a,b,c,x[i+ 3],10,-1894986606);
+            c = ii(c,d,a,b,x[i+10],15,   -1051523); b = ii(b,c,d,a,x[i+ 1],21,-2054922799); a = ii(a,b,c,d,x[i+ 8], 6, 1873313359);
+            d = ii(d,a,b,c,x[i+15],10,  -30611744); c = ii(c,d,a,b,x[i+ 6],15,-1560198380); b = ii(b,c,d,a,x[i+13],21, 1309151649);
+            a = ii(a,b,c,d,x[i+ 4], 6, -145523070); d = ii(d,a,b,c,x[i+11],10,-1120210379); c = ii(c,d,a,b,x[i+ 2],15,  718787259);
+            b = ii(b,c,d,a,x[i+ 9],21, -343485551); 
+            a = ad(a, olda); b = ad(b, oldb); c = ad(c, oldc); d = ad(d, oldd);
+        }
+        return rh(a) + rh(b) + rh(c) + rh(d);
+    },
+    
+    
     /**
      * Show all accessible properties of the passed argument.
      *
      * @param  mixed arg
-     * @param  bool  sort [optional] - whether or not to sort the displayed properties (default: yes)
+     * @param  bool  sort [optional] - whether to sort the displayed properties (default: yes)
      */
     showProperties: function showProperties(arg, sort) {
         if (arg === undefined)                            return alert('rosasurfer.showProperties()\n\nPassed parameter: undefined');
@@ -312,15 +363,15 @@ var rosasurfer = {
 
         var property='', properties=[], type=this.getType(arg);
         sort = (sort===undefined) ? (type!='Array') : Boolean(sort);
-        
+
         for (var i in arg) {
             try {
-                property = ''+ arg[i];                                                                       
-                if ((i=='innerHTML' || i=='textContent') && (property=property.replace(/\n/g, ' ')) && property.length > 100) {   
+                property = ''+ arg[i];
+                if ((i=='innerHTML' || i=='textContent') && (property=property.replace(/\n/g, ' ')) && property.length > 100) {
                     property = property.substr(0, 100) +'...';                                      // limit long HTML contents
                 }
-                else if (property.startsWith('function ') && property.contains('[native code]')) {  // remove line breaks from native function bodies 
-                    property =  property.replace(/\n/g, ' ');                                        
+                else if (property.startsWith('function ') && property.contains('[native code]')) {  // remove line breaks from native function bodies
+                    property =  property.replace(/\n/g, ' ');
                 }
                 property = type +'.'+ i +' = '+ property;
             }
@@ -371,7 +422,7 @@ var rosasurfer = {
         }
         if      (target=='top'   ) div.style.position = 'absolute';
         else if (target=='bottom') div.style.position = 'relative';
-        
+
         if (msg === null)                  msg = '(null)';
         else if (typeof(msg)=='undefined') msg = '(undefined)';
         else                               msg = msg.toString().replace(/ {2,}/g, function(match) { return '&nbsp;'.repeat(match.length); })
