446d718877260749199b482f4c867a85fac664d4
Peter Walther <pewa-github@rosasurfer.com>
2024-07-17 00:11:49 +0300

    replace custom phpstan by Composer dependency

--- etc/phpstan/0.9/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-include(__DIR__.'/../../../src/load.php');
-include(__DIR__.'/DynamicReturnType.php');
-include(__DIR__.'/DAO_Find_ReturnType.php');
-include(__DIR__.'/DAO_FindAll_ReturnType.php');
-include(__DIR__.'/Di_Set_ReturnType.php');
-include(__DIR__.'/PersistableObject_Dao_ReturnType.php');
-include(__DIR__.'/Singleton_GetInstance_ReturnType.php');
