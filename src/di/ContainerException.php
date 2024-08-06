09db3ddf772bbf6066d2ac4ea50a9e2b69cf85fa
Peter Walther <pewa-github@rosasurfer.com>
2024-08-06 08:45:54 +0300

    typo

--- src/di/ContainerException.php
+++ src/di/ContainerException.php
@@ -6,7 +6,7 @@ use Psr\Container\ContainerExceptionInterface;
 
 
 /**
- * NotFoundException
+ * ContainerException
  */
 class ContainerException extends RuntimeException implements ContainerExceptionInterface {
 }
