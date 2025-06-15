74d6b8384b5197a2dc17624d3ca9d0f6981c7be2
Peter Walther <pewa-github@rosasurfer.com>
2025-06-15 13:38:32 +0300

    fix ErrorHandler::setExceptionProperties()

--- src/core/error/ErrorHandler.php
+++ src/core/error/ErrorHandler.php
@@ -793,7 +793,7 @@ class ErrorHandler extends StaticClass {
 
 
     /**
-     * Set the properties of an {@link Exception}.
+     * Set the properties of a {@link Throwable}.
      *
      * @param  Throwable $exception       - exception to modify
      * @param  array[]   $trace           - stacktrace
@@ -807,24 +807,26 @@ class ErrorHandler extends StaticClass {
      * @see \rosasurfer\ministruts\phpstan\STACKFRAME
      */
     private static function setExceptionProperties(Throwable $exception, array $trace, string $file = '', int $line = 0): void {
-        static $traceProperty = null;
-        if (!$traceProperty) {
-            $traceProperty = new ReflectionProperty(Throwable::class, 'trace');
-            $traceProperty->setAccessible(true);
-        }
-        static $fileProperty = null;
-        if (!$fileProperty) {
-            $fileProperty = new ReflectionProperty(Throwable::class, 'file');
-            $fileProperty->setAccessible(true);
-        }
-        static $lineProperty = null;
-        if (!$lineProperty) {
-            $lineProperty = new ReflectionProperty(Throwable::class, 'line');
-            $lineProperty->setAccessible(true);
+        // Throwable is either Error or Exception
+        $className = get_class($exception);
+        while ($parent = get_parent_class($className)) {
+            $className = $parent;
         }
 
+        $traceProperty = new ReflectionProperty($className, 'trace');
+        $traceProperty->setAccessible(true);
         $traceProperty->setValue($exception, $trace);
-        if (func_num_args() > 2) $fileProperty->setValue($exception, $file);
-        if (func_num_args() > 3) $lineProperty->setValue($exception, $line);
+
+        if (func_num_args() > 2) {
+            $fileProperty = new ReflectionProperty($className, 'file');
+            $fileProperty->setAccessible(true);
+            $fileProperty->setValue($exception, $file);
+        }
+
+        if (func_num_args() > 3) {
+            $lineProperty = new ReflectionProperty($className, 'line');
+            $lineProperty->setAccessible(true);
+            $lineProperty->setValue($exception, $line);
+        }
     }
 }
