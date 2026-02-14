9f0bcad510a31746bbf9cf50c1be9c6635a038b5
Peter Walther <pewa-github@rosasurfer.com>
2026-02-14 14:03:42 +0200

    update unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,16 +3,22 @@ declare(strict_types=1);
 
 use rosasurfer\ministruts\Application;
 
-// register the class loader (needed if a global PHPUnit is used)
-require __DIR__.'/../vendor/autoload.php';
+$rootDir = dirname(__DIR__);
 
-// update "php.ini" settings
+// class loader (needed if a global PHPUnit is used)
+require $rootDir.'/vendor/autoload.php';
+
+// php.ini settings
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
-ini_set('error_log', __DIR__.'/../php-error.log');
+ini_set('error_log',          $rootDir.'/phpunit-error.log');
 ini_set('log_errors',         '1');
 ini_set('log_errors_max_len', '0');
 ini_set('memory_limit',      '-1');
+set_include_path($rootDir.'/vendor');
 
 // create an application to initialize the service container
-$app = new Application();
+return new Application([
+    'app.dir.root' => $rootDir,
+    'app.dir.tmp'  => $rootDir.'/etc/tmp',
+]);
--- tests/config/PropertyConfigTest.php
+++ tests/config/PropertyConfigTest.php
@@ -5,21 +5,24 @@ namespace rosasurfer\ministruts\tests\config;
 
 use rosasurfer\ministruts\config\PropertyConfig;
 use rosasurfer\ministruts\core\exception\RuntimeException;
+use rosasurfer\ministruts\core\proxy\Config;
 use rosasurfer\ministruts\tests\helper\TestCase;
+
+use const rosasurfer\ministruts\NL;
 
 /**
  *
  */
 class PropertyConfigTest extends TestCase {
 
-    private string $configFile;
+    protected string $configFile;
 
 
     protected function setUp(): void {
         parent::setUp();
 
-        $this->configFile = tempnam(sys_get_temp_dir(), 'ministruts-config-');
-        file_put_contents($this->configFile, implode("\n", [
+        $this->configFile = tempnam(Config::string('app.dir.tmp', sys_get_temp_dir()), 'ministruts-config-');
+        file_put_contents($this->configFile, join(NL, [
             'int.a = 42',
             'int.b = -7',
             'float.a = 3.14',
@@ -32,32 +35,14 @@ class PropertyConfigTest extends TestCase {
 
 
     protected function tearDown(): void {
-        if (isset($this->configFile) && is_file($this->configFile)) {
+        if (is_file($this->configFile)) {
             unlink($this->configFile);
         }
         parent::tearDown();
     }
 
 
-    public function testIntInterpretsStringRepresentations(): void {
-        $config = new PropertyConfig([$this->configFile]);
-
-        $this->assertSame(42, $config->int('int.a'));
-        $this->assertSame(-7, $config->int('int.b'));
-        $this->assertSame(5, $config->int('missing.key', 5));
-    }
-
-
-    public function testFloatInterpretsStringRepresentations(): void {
-        $config = new PropertyConfig([$this->configFile]);
-
-        $this->assertSame(3.14, $config->float('float.a'));
-        $this->assertSame(-2_500.0, $config->float('float.b'));
-        $this->assertSame(1.25, $config->float('missing.key', 1.25));
-    }
-
-
-    public function testBoolInterpretsStringRepresentations(): void {
+    public function testGetBool(): void {
         $config = new PropertyConfig([$this->configFile]);
 
         $this->assertTrue($config->bool('bool.true'));
@@ -66,10 +51,30 @@ class PropertyConfigTest extends TestCase {
     }
 
 
-    public function testTypedGettersRejectInvalidRepresentations(): void {
+    public function testGetInteger(): void {
         $config = new PropertyConfig([$this->configFile]);
+
+        $this->assertSame(42, $config->int('int.a'));
+        $this->assertSame(-7, $config->int('int.b'));
+        $this->assertSame(5, $config->int('missing.key', 5));
 
         $this->expectException(RuntimeException::class);
         $config->int('text');
+    }
+
+
+    public function testGetFloat(): void {
+        $config = new PropertyConfig([$this->configFile]);
+
+        $this->assertSame(3.14, $config->float('float.a'));
+        $this->assertSame(-2_500.0, $config->float('float.b'));
+        $this->assertSame(1.25, $config->float('missing.key', 1.25));
+    }
+
+
+    public function testGetString(): void {
+        $config = new PropertyConfig([$this->configFile]);
+
+        $this->assertSame('abc', $config->string('text'));
     }
 }
